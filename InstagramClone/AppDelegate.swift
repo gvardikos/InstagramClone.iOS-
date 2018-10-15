8cdd12c44d213af14e9769e0843ce3a2efb3d9ef
gvardikos <giorgosvardikos@gmail.com>
2018-10-15 11:07:57 +0300

    Tab controller + User profile / fetch user

--- InstagramClone/AppDelegate.swift
+++ InstagramClone/AppDelegate.swift
@@ -11,13 +11,15 @@ import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
         FirebaseApp.configure()
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = MainTabBarController()
+        window?.makeKeyAndVisible()
         
         return true
     }
